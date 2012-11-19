50bf485f2d2b6885bfec10de4fab4a35e53c3682
Yanick <Yanick.eberle@students.fhnw.ch>
2012-11-19 01:18:35 +0100

    perl

--- lab1/aufg5/nslookup.pl
+++ lab1/aufg5/nslookup.pl
@@ -1,12 +1,11 @@
 #! usr/bin/perl
 use IO::Socket;
-$b_net = "147.86.";
-for ($i=0; $i<255; $i++) {
-for ($j=0; $j<255; $j++) {
+$b_net = "147.86";
+for ($i=3; $i<4; $i++) {
+for ($j=88; $j<89; $j++) {
 $ipaddr = "$b_net.$i.$j";
-$name = gethostbyaddr($ipaddr, AF_INET);
-if ($name) {
-print "${ip}\t${name}\n";
+$name = gethostbyaddr(${ipaddr}, AF_INET);
+if ($name){
 open (MYFILE, '>>data.txt');
-print MYFILE "${ip}\t${name}\n";
+print MYFILE "${ipaddr} & ${name} \\\\ \n";
 }}}
